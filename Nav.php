3883b4dbe73405fc11ea98970f0252c6b62c4d10
tona-lc <nvtoan@lockon-vn.com>
2018-09-24 09:24:49 +0700

    Init source

---
+++ Nav.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\FlashSale;
+
+use Eccube\Common\EccubeNav;
+
+class Nav implements EccubeNav
+{
+    /**
+     * @return array
+     */
+    public static function getNav()
+    {
+        return [];
+    }
+}
---
+++ TwigBlock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\FlashSale;
+
+use Eccube\Common\EccubeTwigBlock;
+
+class TwigBlock implements EccubeTwigBlock
+{
+    /**
+     * @return array
+     */
+    public static function getTwigBlock()
+    {
+        return [];
+    }
+}
