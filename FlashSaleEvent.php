f109b364346c5475334cf82d0bbf9ed202c5fe2f
tona-lc <nvtoan@lockon-vn.com>
2018-10-02 10:07:58 +0700

    Add discount message to product list

--- FlashSaleEvent.php
+++ FlashSaleEvent.php
@@ -2,7 +2,11 @@
 
 namespace Plugin\FlashSale;
 
+use Eccube\Common\EccubeConfig;
+use Eccube\Entity\Product;
+use Eccube\Entity\ProductClass;
 use Eccube\Event\TemplateEvent;
+use Eccube\Twig\Extension\EccubeExtension;
 use Plugin\FlashSale\Repository\FlashSaleRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -11,16 +15,20 @@ class FlashSaleEvent implements EventSubscriberInterface
     /** @var FlashSaleRepository */
     protected $flashSaleRepository;
 
+    /** @var EccubeConfig */
+    protected $eccubeConfig;
+
     /**
      * FlashSaleEvent constructor.
      *
      * @param FlashSaleRepository $flashSaleRepository
+     * @param EccubeConfig $eccubeConfig
      */
-    public function __construct(FlashSaleRepository $flashSaleRepository)
+    public function __construct(FlashSaleRepository $flashSaleRepository, EccubeConfig $eccubeConfig)
     {
         $this->flashSaleRepository = $flashSaleRepository;
+        $this->eccubeConfig = $eccubeConfig;
     }
-
 
     /**
      * @return array
@@ -29,6 +37,7 @@ class FlashSaleEvent implements EventSubscriberInterface
     {
         return [
             'Product/detail.twig' => 'detail',
+            'Product/list.twig' => 'list',
         ];
     }
 
@@ -42,11 +51,56 @@ class FlashSaleEvent implements EventSubscriberInterface
             $event->addSnippet('@FlashSale/default/detail.twig');
         }
 
-        /*
         $parameters = $event->getParameters();
-        $parameters['ProductReviews'] = $ProductReviews;
-        $parameters['ProductReviewAvg'] = $avg;
-        $parameters['ProductReviewCount'] = $count;
-        $event->setParameters($parameters);*/
+        /** @var Product $Product */
+        $Product = $parameters['Product'];
+        $ProductClasses = $Product->getProductClasses();
+
+        $json = [];
+        $locale = $this->eccubeConfig['locale'];
+        $currency = $this->eccubeConfig['currency'];
+        $formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+        /** @var ProductClass $ProductClass */
+        foreach ($ProductClasses as $ProductClass) {
+            $per = 20;
+            $json[$ProductClass->getId()] = [
+                'message' => '<p><span>'.$formatter->formatCurrency($ProductClass->getPrice02IncTax() * ((100 - $per)/100), $currency).'</span> (-'.$per.'%)</p>'
+            ];
+        }
+
+        $parameters['ProductFlashSale'] = json_encode($json);
+        $event->setParameters($parameters);
+    }
+
+    /**
+     * @param TemplateEvent $event
+     */
+    public function list(TemplateEvent $event)
+    {
+        $CurrentEvent = $this->flashSaleRepository->getCurrentEvent();
+        if ($CurrentEvent) {
+            $event->addSnippet('@FlashSale/default/list.twig');
+        }
+
+        $parameters = $event->getParameters();
+        $pagination = $parameters['pagination'];
+        $json = [];
+        /** @var Product $Product */
+        foreach ($pagination->getItems() as $Product){
+            $ProductClasses = $Product->getProductClasses();
+            $locale = $this->eccubeConfig['locale'];
+            $currency = $this->eccubeConfig['currency'];
+            $formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+            /** @var ProductClass $ProductClass */
+            foreach ($ProductClasses as $ProductClass) {
+                $per = 20;
+                $json[$ProductClass->getId()] = [
+                    'message' => '<p><span>'.$formatter->formatCurrency($ProductClass->getPrice02IncTax() * ((100 - $per)/100), $currency).'</span> (-'.$per.'%)</p>'
+                ];
+            }
+        }
+
+        $parameters['ProductFlashSale'] = json_encode($json);
+        $event->setParameters($parameters);
     }
 }
